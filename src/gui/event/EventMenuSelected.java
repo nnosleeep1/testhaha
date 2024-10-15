f142ab058bfbb4017c810908d9a035e7aa6cd925
TruongXt <truongxt2501@gmail.com>
2024-10-15 22:16:31 +0700

    cap nhat giao dien

---
+++ src/gui/event/EventMenuSelected.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package gui.event;
+
+/**
+ *
+ * @author User
+ */
+public interface EventMenuSelected {
+    public void menuSelected(int menuIndex, int subMenuIndex);
+}
---
+++ src/gui/swing/MenuItem.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package gui.swing;
+
+import gui.event.EventMenu;
+import gui.event.EventMenuSelected;
+import gui.model.ModelMenu;
+import gui.swing.MenuButton;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import net.miginfocom.swing.MigLayout;
+
+/**
+ *
+ * @author User
+ */
+public class MenuItem extends javax.swing.JPanel {
+
+    public ModelMenu getMenu() {
+        return menu;
+    }
+
+    public void setAlpha(float alpha) {
+        this.alpha = alpha;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public EventMenuSelected getEventSelected() {
+        return eventSelected;
+    }
+
+    public void setEventSelected(EventMenuSelected eventSelected) {
+        this.eventSelected = eventSelected;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    private float alpha;
+    private ModelMenu menu;
+    private boolean open;
+    private EventMenuSelected eventSelected;
+    private int index;
+
+    public MenuItem(ModelMenu menu, EventMenu event, EventMenuSelected eventSelected, int index) {
+        initComponents();
+        this.menu = menu;
+        this.eventSelected = eventSelected;
+        this.index = index;
+        setOpaque(false);
+        setLayout(new MigLayout("wrap, fillx, insets 0", "[fill]", "[fill, 20!]0[fill, 35!]"));
+        MenuButton firstItem = new MenuButton(menu.getIcon(), "      " + menu.getMenuName());
+        firstItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (menu.getSubMenu().length > 0) {
+                    if (event.menuPressed(MenuItem.this, !open)) {
+                        open = !open;
+                        
+                    }
+                }
+                eventSelected.menuSelected(index, -1);
+            }
+        });
+        add(firstItem);
+        int subMenuIndex = -1;
+        for (String st : menu.getSubMenu()) {
+            MenuButton item = new MenuButton(st);
+            item.setIndex(++subMenuIndex);
+            item.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    eventSelected.menuSelected(index, item.getIndex());
+                }
+            });
+            add(item);
+        }
+    }
+    
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblIcon = new javax.swing.JLabel();
+        lblName = new javax.swing.JLabel();
+
+        lblIcon.setForeground(new java.awt.Color(255, 255, 255));
+
+        lblName.setForeground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(lblIcon)
+                .addGap(18, 18, 18)
+                .addComponent(lblName)
+                .addContainerGap(252, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(lblIcon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addComponent(lblName, javax.swing.GroupLayout.DEFAULT_SIZE, 263, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lblIcon;
+    private javax.swing.JLabel lblName;
+    // End of variables declaration//GEN-END:variables
+}
